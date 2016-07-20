c2ead9eda23622b7e477a54a55183430891032f4
djb15 <dirk.judd@live.com>
2016-07-20 18:51:48 +0100

    Sidebar on right and map fullscreen

--- static/app.js
+++ static/app.js
@@ -114,5 +114,6 @@ $(function () {
   $("#menu-toggle").click(function(e) {
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
+      $("#menu-toggle").toggleClass("toggled");
   });
 });
